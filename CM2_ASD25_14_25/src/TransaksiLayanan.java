2f5a5499746c27b0c1c6dca9b6fc59afd49d80a3
Rayhwcakep <rayhanindra290805@gmail.com>
2025-05-28 08:36:38 +0700

    Class_TransaksiLayanan

--- CM2_ASD25_14_25/src/TransaksiLayanan.java
+++ CM2_ASD25_14_25/src/TransaksiLayanan.java
@@ -1,3 +1,22 @@
 public class TransaksiLayanan {
-    
+    Pasien pasien;
+    Dokter dokter;
+    int durasiLayanan;
+    int biaya;
+
+    public TransaksiLayanan(Pasien pasien, Dokter dokter, int durasiLayanan, int biaya) {
+        this.pasien = pasien;
+        this.dokter = dokter;
+        this.durasiLayanan = durasiLayanan;
+        this.biaya = biaya;
+    }
+
+    public int hitungBiaya() {
+        return durasiLayanan * 10000;
+    }
+
+    public void tampilkanInformasi() {
+        System.out.println("Pasien: " + pasien.nama + ", Dokter: " + dokter.nama + ", Durasi: "
+                + durasiLayanan + " menit, Biaya: Rp" + biaya);
+    }
 }
