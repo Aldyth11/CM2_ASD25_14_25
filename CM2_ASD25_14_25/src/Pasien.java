6db06a7c08eeb0ecf1bc8a825223c52905258873
Aldyth11 <aldyth.r.f@gmail.com>
2025-05-28 10:27:12 +0700

    revisi

--- CM2_ASD25_14_25/src/Pasien.java
+++ CM2_ASD25_14_25/src/Pasien.java
@@ -3,7 +3,7 @@ public class Pasien {
     String nik;
     String keluhan;
 
-    public Pasien(String nama, String nik,  String keluhan) {
+    public Pasien(String nama, String nik, String keluhan) {
         this.nama = nama;
         this.nik = nik;
         this.keluhan = keluhan;
