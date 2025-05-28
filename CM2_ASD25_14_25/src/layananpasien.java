11252bde170dc6b9031233ce3da7030ef452232d
Rayhwcakep <rayhanindra290805@gmail.com>
2025-05-28 10:09:43 +0700

    Method_TambaPasien

--- CM2_ASD25_14_25/src/layananpasien.java
+++ CM2_ASD25_14_25/src/layananpasien.java
@@ -1,3 +1,29 @@
 public class layananpasien {
-    
+    Node head;
+    Node tail;
+    int size;
+
+    public layananpasien() {
+        this.head = null;
+        this.size = 0;
+        this.tail = null;
+    }
+
+    public boolean isEmpty() {
+        return head == null;
+    }
+
+    public void tambahPasien(String nama, String nik,  String keluhan) {
+        Pasien newPasien = new Pasien(nama, nik, keluhan);
+        Node newNode = new Node(head, size, head);
+        if (isEmpty()) {
+            System.out.println("Antrian Pasien Kosong");
+        } else {
+        tail.next = newNode;
+        newNode.prev = tail;
+        tail = newNode;
+
+        }
+        size++;
+    }
 }
